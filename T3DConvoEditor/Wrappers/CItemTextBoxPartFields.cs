7cfc158070616414e5fa94d478f0bbb21b294905
AGI\rranft <richard.ranft@scientificgames.com>
2017-11-22 10:49:12 -0800

    Committing file updates/changes

---
+++ T3DConvoEditor/Wrappers/CItemTextBoxPartFields.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graph;
+using Graph.Compatibility;
+using Graph.Items;
+using System.Runtime.Serialization;
+
+namespace T3DConvoEditor.Wrappers
+{
+    class CItemTextBoxPartFields : CItemPartFields
+    {
+        public String Text;
+
+        public CItemTextBoxPartFields(ItemTextBoxPart itemPart, ObjectIDGenerator idGen)
+            : base(itemPart as ItemPart, idGen)
+        {
+            Text = itemPart.Text;
+        }
+    }
+}
